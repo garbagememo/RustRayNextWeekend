dea87ed68642cafe8bb9647230c49737a5f3987e
garbagememo <66831242+garbagememo@users.noreply.github.com>
2025-11-01 12:56:07 +0900

    BVHのナイーブな実装。
    Geminiさんで大幅に助けてもらった

--- src/raymod/mod.rs
+++ src/raymod/mod.rs
@@ -3,12 +3,14 @@ mod rayunit;
 mod optarg;
 mod material;
 mod aabb;
+mod bvh;
 pub use self::vec3::*;
 pub use self::rayunit::*;
 pub use self::optarg::*;
 pub use self::material::*;
 pub use self::aabb::*;
-
+pub use self::bvh::*;
+    
 pub const EPS: f64 = 1e-6;
 pub const INF: f64 = 1e20;
 pub const FRAC_SQRT_3: f64 = 1.732050807568877293527446341505872367;
