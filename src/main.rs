650b03258acdb45b38e7eb9f3eb094599426dce3
garbagememo <66831242+garbagememo@users.noreply.github.com>
2025-11-02 01:13:22 +0900

    Next Weekend　第５章まで

--- src/main.rs
+++ src/main.rs
@@ -27,68 +27,86 @@ fn ray_color(r: &Ray, world: &dyn Shape, depth: i64) -> Vec3 {
 }
 
 impl ShapeList {
-    pub fn random_scene(&mut self) {
-
-        let mut box_list1: Vec<Box<dyn Shape>> = Vec::new();
-
-        box_list1.push(Box::new(Sphere::new(
-            Vec3::new(0.0, -1000.0, 0.0),
-            1000.0,
-            Arc::new(Lambertian::new(Vec3::new(0.5, 0.5, 0.5))),
+    pub fn simple_scene(&mut self) {
+        self.push(Box::new(Sphere::new(
+            Vec3::new(0.6, 0.0, -1.0),
+            0.5,
+            Arc::new(Lambertian::new(Box::new(ColorTexture::new(Vec3::new(
+                0.1, 0.2, 0.5,
+            ))))),
         )));
-
-        for a in -11..11 {
-            for b in -11..11 {
-                let choose_mat = random();
-                let center = Vec3::new(a as f64 + 0.9 * random(), 0.2, b as f64 + 0.9 * random());
-                if (center - Vec3::new(4.0, 0.2, 0.0)).length().sqrt() > 0.9 {
-                    if choose_mat < 0.8 {
-                        // diffuse
-                        let albedo = Vec3::random().mult(Vec3::random());
-                        box_list1.push(Box::new(Sphere::new(
-                            center,
-                            0.2,
-                            Arc::new(Lambertian::new(albedo)),
-                        )));
-                    } else if choose_mat < 0.95 {
-                        // Metal
-                        let fuzz = random_range(0.0, 0.5);
-                        let albedo = Vec3::vec3_random_range(0.5, 1.0);
-                        box_list1.push(Box::new(Sphere::new(
-                            center,
-                            0.2,
-                            Arc::new(Metal::new(albedo, fuzz)),
-                        )));
-                    } else {
-                        // glass
-                        box_list1.push(Box::new(Sphere::new(
-                            center,
-                            0.2,
-                            Arc::new(Dielectric::new(1.5)),
-                        )));
+        self.push(Box::new(Sphere::new(
+            Vec3::new(-0.6, 0.0, -1.0),
+            0.5,
+            Arc::new(Metal::new(
+                Box::new(ColorTexture::new(Vec3::new(0.8, 0.8, 0.8))),
+                0.4,
+            )),
+        )));
+        self.push(Box::new(Sphere::new(
+            Vec3::new(0.0, -100.5, 0.0),
+            100.0,
+            Arc::new(Lambertian::new(Box::new(CheckerTexture::new(
+                Box::new(ColorTexture::new(Vec3::new(0.8, 0.8, 0.0))),
+                Box::new(ColorTexture::new(Vec3::new(0.8, 0.2, 0.0))),
+                10.0,
+            )))),
+        )));
+    }
+    /*
+        pub fn random_scene(&mut self){
+            self.push(Box::new(Sphere::new(
+                Vec3::new(0.0, -1000.0, 0.0),
+                1000.0,
+                Arc::new(Lambertian::new(Vec3::new(0.5, 0.5, 0.5))),
+            )));
+            for a in -11..11 {
+                for b in -11..11 {
+                    let choose_mat=random();
+                    let center=Vec3::new(a as f64 + 0.9 * random(), 0.2, b as f64 + 0.9 * random() );
+                    if (center-Vec3::new(4.0,0.2,0.0)).length().sqrt() > 0.9 {
+                        if choose_mat < 0.8 {
+                            // diffuse
+                            let albedo = Vec3::random().mult(Vec3::random() );
+                            self.push(Box::new(Sphere::new(
+                                center,
+                                0.2,
+                                Arc::new(Lambertian::new(albedo)),
+                            )));
+                        }else if choose_mat <0.95{
+                            // Metal
+                            let fuzz= random_range(0.0,0.5);
+                            let albedo=Vec3::vec3_random_range(0.5,1.0);
+                            self.push(Box::new(Sphere::new(
+                                center,
+                                0.2,
+                                Arc::new(Metal::new(albedo,fuzz)),
+                            )));
+                        } else {
+                            // glass
+                            self.push(Box::new(Sphere::new(
+                                center,
+                                0.2,
+                                Arc::new(Dielectric::new(1.5)),
+                            )));
+                        }
                     }
                 }
             }
+            self.push(Box::new(Sphere::new(
+                Vec3::new(0.0,1.0,0.0),
+                1.0,
+                Arc::new(Dielectric::new(1.5)),)));
+            self.push(Box::new(Sphere::new(
+                Vec3::new(-4.0,1.0,0.0),
+                1.0,
+                Arc::new(Lambertian::new(Vec3::new(0.4,0.2,0.1)),))));
+            self.push(Box::new(Sphere::new(
+                Vec3::new(4.0,1.0,0.0),
+                1.0,
+                Arc::new(Metal::new(Vec3::new(0.7,0.6,0.5),0.0),))));
         }
- 
-        box_list1.push(Box::new(Sphere::new(
-            Vec3::new(0.0, 1.0, 0.0),
-            1.0,
-            Arc::new(Dielectric::new(1.5)),
-        )));
-        box_list1.push(Box::new(Sphere::new(
-            Vec3::new(-4.0, 1.0, 0.0),
-            1.0,
-            Arc::new(Lambertian::new(Vec3::new(0.4, 0.2, 0.1))),
-        )));
-        box_list1.push(Box::new(Sphere::new(
-            Vec3::new(4.0, 1.0, 0.0),
-            1.0,
-            Arc::new(Metal::new(Vec3::new(0.7, 0.6, 0.5), 0.0)),
-        )));
-
-        self.push(Box::new(BVH::new(box_list1)));
-   }
+    */
 }
 
 fn main() {
@@ -103,10 +121,16 @@ fn main() {
     let mut image = vec![Color::zero(); (w * h) as usize];
 
     // Camera
-
-    let lookfrom = Vec3::new(13.0, 2.0, 3.0);
-    let lookat = Vec3::new(0.0, 0.0, 0.0);
+    /* random_scene用カメラ
+       let lookfrom = Vec3::new(13.0, 2.0, 3.0);
+       let lookat = Vec3::new(0.0, 0.0, 0.0);
+       let vup = Vec3::new(0.0, 1.0, 0.0);
+    */
+    // simple_scene用カメラ
+    let lookfrom = Vec3::new(0.0, 1.0, 4.0);
+    let lookat = Vec3::new(0.0, 0.0, -1.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
+
     let dist_to_focus = (lookfrom - lookat).length().sqrt();
     let aperture = 0.1;
 
@@ -123,19 +147,19 @@ fn main() {
     let MAX_DEPTH: i64 = 32;
 
     let mut world = ShapeList::new();
-    world.random_scene();
+    world.simple_scene();
 
     let bands: Vec<(usize, &mut [Color])> = image.chunks_mut(w as usize).enumerate().collect();
     bands.into_par_iter().for_each(|(y, band)| {
         for x in 0..w {
             let mut r = Vec3::new(0.0, 0.0, 0.0);
-            for _sx in 0..2 {
-                for _sy in 0..2{
-                    for _spp in 0..samps {
-                        let u = (x as f64 +(_sx as f64/2.0 )+ random()/2.0 ) / (w as f64);
-                        let v = (y as f64 +(_sy as f64/2.0 )+ random()/2.0 ) / (h as f64);
+            for _spp in 0..samps {
+                for _sy in 0..2 {
+                    for _sx in 0..2 {
+                        let u = (x as f64 + (_sx as f64 + random()) / 4.0) / (w as f64);
+                        let v = (y as f64 + (_sy as f64 + random()) / 4.0) / (h as f64);
                         let ray = cam.get_ray(u, v);
-                        r = r + ray_color(&ray, &world, MAX_DEPTH) / (samps as f64)/4.0;
+                        r = r + ray_color(&ray, &world, MAX_DEPTH) / (samps as f64) / 4.0;
                     }
                 }
             }
